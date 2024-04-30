0319dc8ec4f2fbfe351b4be576b52f5673eefcf5
ykkkkk0215 <168529522+ykkkkk0215@users.noreply.github.com>
2024-04-30 18:58:07 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all  # すべてのレコードを@postsに代入  end
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+    redirect_to "/posts"
+  end
+end
+
